a60e85ce9a63aac5c9c0a6bfded5c79d849eeeb5
Brent J Sullivan <brntsllvn@gmail.com>
2015-05-22 14:25:02 +0000

    add devise views and migrations:

--- db/migrate/20150522142130_add_devise_to_users.rb
+++ db/migrate/20150522142130_add_devise_to_users.rb
@@ -1,6 +1,6 @@
-class AddDeviseToPeople < ActiveRecord::Migration
+class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
-    change_table(:people) do |t|
+    change_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -35,10 +35,10 @@ class AddDeviseToPeople < ActiveRecord::Migration
       # t.timestamps
     end
 
-    add_index :people, :email,                unique: true
-    add_index :people, :reset_password_token, unique: true
-    # add_index :people, :confirmation_token,   unique: true
-    # add_index :people, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522141026) do
+ActiveRecord::Schema.define(version: 20150522142130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "availabilities", force: :cascade do |t|
-    t.string   "name"
-    t.string   "walker_sitter"
-    t.datetime "available_from"
-    t.string   "available_location"
-    t.datetime "available_to"
-    t.string   "dog_size"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "person_id"
-  end
-
-  add_index "availabilities", ["person_id"], name: "index_availabilities_on_person_id", using: :btree
 
   create_table "jobs", force: :cascade do |t|
     t.string   "name"
@@ -69,13 +55,25 @@ ActiveRecord::Schema.define(version: 20150522141026) do
     t.string   "last_name"
     t.string   "building"
     t.string   "address"
-    t.string   "email"
     t.string   "phone"
     t.string   "emergency_phone"
     t.boolean  "parent"
     t.boolean  "walker"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
