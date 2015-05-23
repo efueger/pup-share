b4e3117634dc7ebda7af2d20643dfc65ce87e5f4
Brent J Sullivan <brntsllvn@gmail.com>
2015-05-23 20:53:02 +0000

    add confirmable to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   #validates :first_name,            presence: true, length: { minimum: 2, maximum: 50 }
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   #validates :address,               presence: true, length: { minimum: 2, maximum: 100 }  
   #validates :phone,                 presence: true # consider using the 'format' validation later
   #validates :emergency_phone,       presence: true # consider using the 'format' validation later
-  validates_format_of :email, :with => /.{1,40}@amazon.com/, message: "Email addres must have an @amazon.com extension", on: :create             
+  #validates_format_of :email, :with => /.{1,40}@amazon.com/, message: "Email addres must have an @amazon.com extension", on: :create             
   
   has_many :jobs  #, dependent: :destroy
 end
