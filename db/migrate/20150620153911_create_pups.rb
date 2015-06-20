73621a5063527bc10247a8fc9d29e449b7a22c2d
Brent J Sullivan <brntsllvn@gmail.com>
2015-06-20 15:47:05 +0000

    create Pup model

--- db/migrate/20150620153911_create_pups.rb
+++ db/migrate/20150620153911_create_pups.rb
@@ -2,6 +2,15 @@ class CreatePups < ActiveRecord::Migration
   def change
     create_table :pups do |t|
 
+      t.string   :pup_name
+      t.string   :pup_breed
+      t.string   :pup_weight
+      t.string   :pup_gender
+      t.string   :pup_vet_phone
+      t.text     :pup_care_instructions
+      t.integer  :pup_age
+      t.integer  :user_id
+
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612025128) do
+ActiveRecord::Schema.define(version: 20150620153911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,19 @@ ActiveRecord::Schema.define(version: 20150612025128) do
   end
 
   add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
+
+  create_table "pups", force: :cascade do |t|
+    t.string   "pup_name"
+    t.string   "pup_breed"
+    t.string   "pup_weight"
+    t.string   "pup_gender"
+    t.string   "pup_vet_phone"
+    t.text     "pup_care_instructions"
+    t.integer  "pup_age"
+    t.integer  "user_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
