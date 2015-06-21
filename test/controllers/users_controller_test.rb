22b917ad38962ddd1862f71c89d4b503187c434e
Brent J Sullivan <brntsllvn@gmail.com>
2015-06-21 17:54:03 +0000

    jobs view minor bug fix

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-#   setup do
-#     @user = users(:one)
-#   end
+  setup do
+    @user = FactoryGirl.create(:user)
+  end
 
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#     assert_not_nil assigns(:users)
-#   end
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
 
 #   test "should get new" do
 #     get :new
