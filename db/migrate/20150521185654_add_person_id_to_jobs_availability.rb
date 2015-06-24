f5174f2783dc63f8cee6610b76dfa4152a95c328
Brent J Sullivan <brntsllvn@gmail.com>
2015-06-24 00:44:48 +0000

    migration bug fix

--- db/migrate/20150521185654_add_person_id_to_jobs_availability.rb
+++ db/migrate/20150521185654_add_person_id_to_jobs_availability.rb
@@ -2,7 +2,7 @@ class AddPersonIdToJobsAvailability < ActiveRecord::Migration
   def change
     add_column :jobs, :person_id, :string
     add_index  :jobs, :person_id
-    add_column :availabilities, :person_id, :string
-    add_index  :availabilities, :person_id
+#     add_column :availabilities, :person_id, :string
+#     add_index  :availabilities, :person_id
   end
 end
