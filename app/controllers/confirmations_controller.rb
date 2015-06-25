774f4d80949b61bb805a72ffec4faae119fb62cd
Brent J Sullivan <brntsllvn@gmail.com>
2015-06-25 01:29:33 +0000

    removed a random pry

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,7 +3,6 @@ class ConfirmationsController < Devise::ConfirmationsController
   protected
   
   def after_confirmation_path_for(resource_name, resource)
-    binding.pry
     flash[:notice]
     sign_in(resource_name, resource)
     root_path
