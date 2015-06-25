7737ca702603fde19d63c641e1faf285dc8b14f2
Brent J Sullivan <brntsllvn@gmail.com>
2015-06-25 15:58:15 +0000

    minor tweaks to devise tester to avoid random binding.prys wrecking my builds

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,8 +1,9 @@
 class ConfirmationsController < Devise::ConfirmationsController
   
-  protected
+  private
   
   def after_confirmation_path_for(resource_name, resource)
+    binding.pry
     flash[:notice]
     sign_in(resource_name, resource)
     root_path
--- test/controllers/confirmations_controller_test.rb
+++ test/controllers/confirmations_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
+require 'pry'
 
 class ConfirmationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+# #   include Devise::TestHelpers
+  
+#   test 'after confirmation redirect to root' do
+#     user = FactoryGirl.create(:user, confirmed_at: nil)   
+#     binding.pry
+#     user.confirm! # works
+#     sign_in user # does not work. i.e. user attributes not updated
+#     assert user_signed_in? # undefined method...?
+#   end
+  
 end
