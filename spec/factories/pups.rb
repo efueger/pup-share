1f6397a746e6e4cf2a2641ff477e6e114059d633
Brent J Sullivan <brntsllvn@gmail.com>
2015-07-10 00:42:15 +0000

    spec pic upload bug fix

--- spec/factories/pups.rb
+++ spec/factories/pups.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
     pup_vet_phone 555-555-5555
     pup_care_instructions 'Watch out! He\'s a jumper'
     pup_age 3
-    pup_pic { Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'fallback', 'pup_walk.png')) }
+    # pup_pic { Rack::Test::UploadedFile.new(File.join(Rails.root, 'app', 'assets', 'images', 'fallback', 'pup_walk.png')) }
     association :user
   end
   
