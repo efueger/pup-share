b920adabbc2c6be74f80c1f6e45bd30352179a0e
Brent J Sullivan <brntsllvn@gmail.com>
2015-07-12 23:33:54 +0000

    change pic size

--- app/uploaders/pup_pic_uploader.rb
+++ app/uploaders/pup_pic_uploader.rb
@@ -1,7 +1,7 @@
 class PupPicUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  process resize_to_fill: [75, 75]
+  process resize_to_fill: [200, 200]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- app/uploaders/user_pic_uploader.rb
+++ app/uploaders/user_pic_uploader.rb
@@ -1,7 +1,7 @@
 class UserPicUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  process resize_to_fill: [75, 75]
+  process resize_to_fill: [200, 200]
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -10,5 +10,4 @@ class UserPicUploader < CarrierWave::Uploader::Base
   def default_url(*args)
     ActionController::Base.helpers.asset_path("fallback/" + [version_name, 'dilbert.jpg'].compact.join('_'))
   end
-
 end
