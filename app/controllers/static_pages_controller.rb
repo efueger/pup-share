19afe511630b2fd78bfaef25dd43ce16e2f22463
Brent J Sullivan <brntsllvn@gmail.com>
2015-07-20 23:08:45 +0000

    add static pages and bottom nav

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,11 @@
 class StaticPagesController < ApplicationController
+  
   def index; end
+  
+  def about; end
+  
+  def terms_of_service; end
+  
+  def privacy_policy; end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     }
 
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 
   resources :users do
     resources :pups
