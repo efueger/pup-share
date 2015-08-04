7eb20a9f8d0f935c414b4145544ce02afaedcc26
Brent J Sullivan <brntsllvn@gmail.com>
2015-08-04 21:33:04 +0000

    add sign-in/up links on critical pages

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,12 +1,11 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
-
   private
 
   def after_confirmation_path_for(resource_name, resource)
     flash[:notice]
     sign_in(resource_name, resource)
-    jobs_path
+    edit_user_path(current_user)
   end  
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,9 @@ class User < ActiveRecord::Base
   # validates :building,              presence: true
   # validates :address,               presence: true, length: { minimum: 2, maximum: 100 }  
   # validates :phone,                 presence: true 
-  # consider using the 'format' validation later
   # validates :emergency_phone,       presence: true 
-  # consider using the 'format' validation later
-  # validates :pic,                   presence: true
 
-  validates_format_of :email, :with => /.{1,50}@amazon.com/, message: "Email address must have @amazon domain", on: :create  
+  validates_format_of :email, :with => /.{1,50}@amazon.com/, message: "Email address must have @amazon.com domain"  
 
   has_many :jobs, dependent: :destroy
   has_many :pups, dependent: :destroy
